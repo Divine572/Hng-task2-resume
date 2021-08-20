883c6e1e59c74322a289c81bbe738725898162ae
Osagie <osagieiyayi09@gmail.com>
2021-08-20 02:17:02 +0100

    implemented the mail sending functionality

--- index.js
+++ index.js
@@ -1,46 +1,38 @@
 const express = require('express');
+const nodemailer = require('nodemailer');
 const mongoose = require('mongoose');
-
+const dotenv = require('dotenv');
+const Contact = require('./models')
+const bodyparser = require('body-parser')
+dotenv.config()
 app = express();
-
-
 
 
 // Middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(bodyparser.urlencoded({ extended: true }))
 
 
 
-mongoose.connect('mongodb://localhost/resume',
-    { useNewUrlParser: true },
-    { useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/resume',
+    { useNewUrlParser: true ,
+     useUnifiedTopology: true,useCreateIndex:true })
     .then(() => console.log('Connected to mongodb..'))
     .catch(err => console.error('Could not connect to mongodb server..'));
 
-
-const contactSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        maxlength: 50,
-        minlength: 5,
-        required: true
-    },
-    email: {
-        type: String,
-        maxlength: 50,
-        minlength: 5,
-        required: true
-    },
-    message: {
-        type: String,
-        maxlength: 250,
-        minlength: 5,
-        required: true
+//create a tranporter object
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: process.env.MAIL_USERNAME,
+      pass: process.env.MAIL_PASSWORD,
+      clientId: process.env.OAUTH_CLIENTID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      refreshToken: process.env.OAUTH_REFRESH_TOKEN
     }
-});
-
-const Contact = mongoose.model('Contact', contactSchema);
+  });
 
 
 
@@ -50,27 +42,44 @@ app.get('/', (req, res) => {
 
 
 app.post('/', async (req, res) => {
+
     try {
         let contact = new Contact({
-            name: req.body.name,
-            email: req.body.email,
-            message: req.body.message     
+          name: req.body.name,
+          email: req.body.email,
+          message: req.body.message   
+      
         })
         await contact.save();
-        res.redirect('/');
+        let mailOptions = {
+            from: process.env.MAIL_USERNAME,
+            to: req.body.email,
+            subject: 'Enquiries As regards my CV',
+            text: req.body.message
+          };
+         transporter.sendMail(mailOptions, function(err, data) {
+            if (err) {
+              console.log("Error " + err);
+            } else {
+              console.log("Email sent successfully", data);
+            }
+          });
+        return res.redirect('/');
     }
     catch (err) {
-        // console.error(err)
+        console.error(err) // for monitoring errors
         // res.render('error/400')
         res.send(err.message);
+        return res.redirect('/')
     } 
+    
 });
 
 
 
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 7000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
